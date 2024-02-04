6fe47637ff43284d1bbf3fe90b3f23de17384c36
Solo150 <130642088+Solo150@users.noreply.github.com>
2024-02-04 17:00:54 +0300

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -67,7 +67,7 @@ export const Badge = (props: Props) => {
           color: props.poweredByTextColor ?? defaultTextColor,
         }}
       >
-        <span> Mix-id</span>
+        <span> Mix-ID</span>
       </a>
     </span>
   );
